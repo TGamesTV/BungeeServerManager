50e58ae1fc0dd49d2b58e54cff5866893ff26a39
DavidoTek <54072917+DavidoTek@users.noreply.github.com>
2019-12-04 19:48:04 +0100

    Create BungeeServerManager.java
    Created Plugin's Main Class

---
+++ src/de/mfgames/BungeeServerManager/BungeeServerManager.java
@@ -0,0 +1,33 @@
+/*
+ * BungeeServerManager v1.0
+ * 
+ * Copyright (C) 2019 TGamesTV
+ * Copyright (C) 2019 DavidoTek
+ * 
+ * Licensed under MIT
+ * 
+ * */
+package de.mfgames.BungeeServerManager;
+
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class BungeeServerManager extends Plugin {
+
+	public static String pver = "1.0";	/* Plugin Version */
+	
+	@Override
+	public void onEnable() {
+		registerCommands();
+		System.out.println("[BungeeServerManager] BungeeServerManager enabled.");
+	}
+	
+	@Override
+	public void onDisable() {
+		System.out.println("[BungeeServerManager] Plugin disabled.");
+	}
+	
+	public void registerCommands() {
+		ProxyServer.getInstance().getPluginManager().registerCommand(this, new COMMAND_bungeeservermanager("bungeeservermanager"));
+	}
+}
