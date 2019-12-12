85a0c12f085b3faeffd8be09f018875567eeb036
DavidoTek <54072917+DavidoTek@users.noreply.github.com>
2019-12-12 20:17:47 +0100

    Added features to /bss

--- src/de/mfgames/BungeeServerManager/COMMAND_bungeeserverstats.java
+++ src/de/mfgames/BungeeServerManager/COMMAND_bungeeserverstats.java
@@ -26,15 +26,18 @@ public class COMMAND_bungeeserverstats extends Command {
 	@Override
 	public void execute(CommandSender sender, String[] args) {
 		/* Check if sender has the permissions (print message and return if not) */
-		if (!sender.hasPermission("bungeeservermanager.stats")) {
+		
+		if (!sender.hasPermission("bungeeserveradmin.stats") && !sender.hasPermission("bungeeserveradmin.*")) {
 			sender.sendMessage(new TextComponent("§cYou do not have the permissions to execute this command!"));
 			return;
 		}
 		
 		OperatingSystemMXBean operatingSystemMXBean = ManagementFactory.getOperatingSystemMXBean();
 		
-		sender.sendMessage(new TextComponent("§6§l ==== SYSTEM STATISTICS ==== "));
-		sender.sendMessage(new TextComponent("§6System CPU usage: " + operatingSystemMXBean.getSystemLoadAverage()));
+		sender.sendMessage(new TextComponent("§6§l ==== SYSTEM INFORMATION ==== "));
+		sender.sendMessage(new TextComponent("§6OS: " + System.getProperty("os.name")));
+		sender.sendMessage(new TextComponent("§CPU: " + operatingSystemMXBean.getSystemLoadAverage()));
+		sender.sendMessage(new TextComponent("§6JVM Memory: " + (Runtime.getRuntime().totalMemory() / 1024 / 1024) + "M"));
 		
 	}
 
