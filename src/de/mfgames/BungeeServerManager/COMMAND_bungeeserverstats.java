9a919d2f51e37303f9dec2afac3c98e5e47a564e
DavidoTek <54072917+DavidoTek@users.noreply.github.com>
2020-01-02 18:59:19 +0100

    Add support for running system commands

--- src/de/mfgames/BungeeServerManager/COMMAND_bungeeserverstats.java
+++ src/de/mfgames/BungeeServerManager/COMMAND_bungeeserverstats.java
@@ -9,7 +9,7 @@
  * */
 package de.mfgames.BungeeServerManager;
 
-
+import java.io.IOException;
 import java.lang.management.ManagementFactory;
 import com.sun.management.OperatingSystemMXBean;
 
@@ -25,21 +25,44 @@ public class COMMAND_bungeeserverstats extends Command {
 
 	@Override
 	public void execute(CommandSender sender, String[] args) {
-		/* Check if sender has the permissions (print message and return if not) */
-		
-		if (!sender.hasPermission("bungeeserveradmin.stats") && !sender.hasPermission("bungeeserveradmin.*")) {
-			sender.sendMessage(new TextComponent("§cYou do not have the permissions to execute this command!"));
+			
+		if (args.length == 0) {	// Show system stats
+			/* Check if sender has the permissions (print message and return if not) */
+			if (!sender.hasPermission("bungeeserveradmin.stats") && !sender.hasPermission("bungeeserveradmin.*")) {
+				sender.sendMessage(new TextComponent("§cYou do not have the permissions to execute this command!"));
+				return;
+			}
+			
+			OperatingSystemMXBean operatingSystemMXBean = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+			
+			sender.sendMessage(new TextComponent("§6§l ==== SYSTEM INFORMATION ==== "));
+			sender.sendMessage(new TextComponent("§6OS: " + System.getProperty("os.name") + " " + System.getProperty("os.arch")));
+			sender.sendMessage(new TextComponent("§6CPU: " + operatingSystemMXBean.getProcessCpuLoad()));
+			sender.sendMessage(new TextComponent("§6JVM: " + System.getProperty("java.vendor") + " " + System.getProperty("java.version")));
+			sender.sendMessage(new TextComponent("§6JVM Memory: " + (Runtime.getRuntime().totalMemory() / 1024 / 1024) + "M"));
+			// don't return: Show info
+		} else if (args.length == 2 && args[0].equalsIgnoreCase("cmd")) {	// Execute command on system console
+			String command = args[1];
+			String commandPerm = BungeeServerManager.getInstance().getConfiguration().getString("system-commands." + command + ".permission");
+			
+			if(!sender.hasPermission(commandPerm)) {
+				sender.sendMessage(new TextComponent("§cYou do not have the permissions to execute this command!"));
+				return;
+			}
+			
+			String commandExec = BungeeServerManager.getInstance().getConfiguration().getString("system-commands." + command + ".execute");
+			
+			try {
+				ProcessBuilder pb = new ProcessBuilder(commandExec);
+				pb.start();
+			} catch (IOException e) {
+				e.printStackTrace();
+				sender.sendMessage(new TextComponent("§cAn error occured!"));
+			}
 			return;
 		}
-		
-		OperatingSystemMXBean operatingSystemMXBean = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
-		
-		sender.sendMessage(new TextComponent("§6§l ==== SYSTEM INFORMATION ==== "));
-		sender.sendMessage(new TextComponent("§6OS: " + System.getProperty("os.name") + " " + System.getProperty("os.arch")));
-		sender.sendMessage(new TextComponent("§6CPU: " + operatingSystemMXBean.getProcessCpuLoad()));
-		sender.sendMessage(new TextComponent("§6JVM: " + System.getProperty("java.vendor") + " " + System.getProperty("java.version")));
-		sender.sendMessage(new TextComponent("§6JVM Memory: " + (Runtime.getRuntime().totalMemory() / 1024 / 1024) + "M"));
-		
+		end:
+		sender.sendMessage(new TextComponent("§aTo execute system command: /bss cmd <command>"));
 	}
 
 }
